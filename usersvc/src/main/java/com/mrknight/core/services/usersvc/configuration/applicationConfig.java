56be936d3648b1dabc0616f3bbb26c03568cd438
jfcaballerop <jfcaballerop@gmail.com>
2022-02-26 12:12:08 +0100

    add Mongo connection

--- usersvc/src/main/java/com/mrknight/core/services/usersvc/configuration/applicationConfig.java
+++ usersvc/src/main/java/com/mrknight/core/services/usersvc/configuration/applicationConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
